1f8be7f1c997508a5d6e6cd72392c5280b8567c4
曹家玮 <529738530@qq.com>
2021-06-16 17:54:55 +0800

    feat: tree

---
+++ 树/Easy/[110]平衡二叉树/solutions.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+/**
+ * 首刷
+ * 思路： 先求高，然后递归比较各个左右子树高度
+ */
+const hight = (root) => {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(hight(root.left), hight(root.right)) + 1;
+};
+var isBalanced = function (root) {
+  if (!root) {
+    return 0;
+  }
+  return Math.abs(isBalanced(root.left) - isBalanced(root.right)) < 1;
+};
