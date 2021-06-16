4468c588410252ca24a0302393d888e85157be8f
曹家玮 <529738530@qq.com>
2021-06-16 17:38:25 +0800

    feat: tree

--- 树/Easy/[108]将有序数组转化为二叉搜索树[x]/solutions.js
+++ 树/Easy/[108]将有序数组转化为二叉搜索树[x]/solutions.js
@@ -11,7 +11,26 @@
  * @return {TreeNode}
  */
 
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
 /**
  * 首刷
  */
-var sortedArrayToBST = function (nums) {};
+var sortedArrayToBST = function (nums) {
+  const dfs = (left, right) => {
+    let root = null;
+    if (left <= right) {
+      const i = (left + right) >> 1;
+      root = new TreeNode(nums[i]);
+      root.left = dfs(left, i - 1);
+      root.right = dfs(i + 1, right);
+    }
+    return root;
+  };
+  return dfs(0, nums.length - 1);
+};
+// >> 带符号右移， >>> 右移， <<左移
