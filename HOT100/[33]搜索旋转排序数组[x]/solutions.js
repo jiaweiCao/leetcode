656dc508e8b7b52c19501ef7e3a85eb121f6b0cd
曹家玮 <529738530@qq.com>
2021-06-21 20:09:20 +0800

    feat: hot100

---
+++ HOT100/[33]搜索旋转排序数组[x]/solutions.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+/**
+ * 首刷
+ * 思路：给定升序列表，首先找出旋转后哪个地方开始打破升序了，那么那个地方就为k+1的位置，
+ */
+var search = function (nums, target) {};
