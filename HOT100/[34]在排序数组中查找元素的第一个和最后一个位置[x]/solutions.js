6ae4e17c132a28aa2cc3405cafc07d5a5f0576b1
曹家玮 <529738530@qq.com>
2021-06-21 21:38:23 +0800

    chore: hoot100(empty)

---
+++ HOT100/[34]在排序数组中查找元素的第一个和最后一个位置[x]/solutions.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {};
+
+console.log(searchRange([1, 2, 3, 3, 3, 3, 4, 5, 9], 3));
+// searchRange([5, 7, 7, 8, 8, 10], 8);
