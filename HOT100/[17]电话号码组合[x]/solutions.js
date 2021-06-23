89f913a7ab1120786553618bb301dc4cd1b5c40d
曹家玮 <529738530@qq.com>
2021-06-23 16:01:44 +0800

    feat: hot100

--- HOT100/[17]电话号码组合[x]/solutions.js
+++ HOT100/[17]电话号码组合[x]/solutions.js
@@ -2,6 +2,33 @@
  * @param {string} digits
  * @return {string[]}
  */
- var letterCombinations = function(digits) {
+var letterCombinations = function (digits) {
+  if (!digits) {
+    return [];
+  }
+  const letterMap = new Map([
+    ["2", "abc"],
+    ["3", "def"],
+    ["4", "ghi"],
+    ["5", "jkl"],
+    ["6", "mno"],
+    ["7", "pqrs"],
+    ["8", "tuv"],
+    ["9", "wxyz"],
+  ]);
+  const result = [];
+  backTracking = (combine, nextIndex) => {
+    if (combine.length === digits.length) {
+      result.push(combine);
+      return;
+    }
 
+    const curDigit = digits[nextIndex];
+    const curLetter = letterMap.get(curDigit);
+    for (let i = 0; i < curLetter.length; i++) {
+      backTracking(combine + curLetter[i], nextIndex + 1);
+    }
+  };
+  backTracking("", 0);
+  return result;
 };
