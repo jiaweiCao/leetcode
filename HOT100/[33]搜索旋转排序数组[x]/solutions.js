0cd7b9909fd6a7641c0ea6c14570dc75c27405dc
曹家玮 <529738530@qq.com>
2021-06-24 21:27:49 +0800

    feat: hot100

--- HOT100/[33]搜索旋转排序数组[x]/solutions.js
+++ HOT100/[33]搜索旋转排序数组[x]/solutions.js
@@ -6,6 +6,15 @@
 
 /**
  * 首刷
- * 思路：给定升序列表，首先找出旋转后哪个地方开始打破升序了，那么那个地方就为k+1的位置，
+ * 思路：这道题O(n)方法直接求解就浪费了这么题干，既然有序，那先用二分法做，每次找到一个区间，一定有一边是升序
  */
-var search = function (nums, target) {};
+var search = function (nums, target) {
+  let result = -1;
+  if (!Array.isArray(nums) || nums.length === 0) {
+    return result;
+  }
+  const binary = (left, right) => {
+    const mid = (left + right) >> 1;
+  };
+  return result;
+};
--- HOT100/[39]组合总数/solutions.js
+++ HOT100/[39]组合总数/solutions.js
@@ -3,4 +3,28 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function (candidates, target) {};
+/**
+ * 首刷
+ * @思路：通过回溯法可以将所有满足条件的穷举出来，但是这里不能有重复的所以还需要加上curIndex
+ */
+var combinationSum = function (candidates, target) {
+  const result = [];
+  const backTracking = (arrs, sum, curIndex) => {
+    let newArr = [...arrs];
+
+    if (sum > target) {
+      return;
+    }
+    if (sum === target) {
+      result.push(newArr);
+      return;
+    }
+
+    for (let i = curIndex; i < candidates.length; i++) {
+      backTracking([...newArr, candidates[i]], sum + candidates[i], i);
+    }
+  };
+  backTracking([], 0, 0);
+  return result;
+};
+console.log(combinationSum([2, 3, 6, 7], 7));
