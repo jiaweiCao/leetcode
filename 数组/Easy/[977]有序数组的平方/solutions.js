c4a7cffed0412dc96cc4276bf2ad05ffed73b376
曹家玮 <529738530@qq.com>
2021-07-01 14:17:44 +0800

    feat: 数组

--- 数组/Easy/[977]有序数组的平方/solutions.js
+++ 数组/Easy/[977]有序数组的平方/solutions.js
@@ -10,3 +10,25 @@ var sortedSquares = function (nums) {
   nums.sort((a, b) => a - b);
   return nums;
 };
+
+// 二刷：双指针
+
+var sortedSquares2 = function (nums) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  const result = [];
+
+  while (start <= end) {
+    if (Math.abs(nums[start]) > Math.abs(nums[end])) {
+      result.unshift(nums[start] * nums[start]);
+      start += 1;
+    } else {
+      result.unshift(nums[end] * nums[end]);
+      end -= 1;
+    }
+  }
+  return result;
+};
+
+console.log(sortedSquares2([-4, -1, 0, 3, 10]));
