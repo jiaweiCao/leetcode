3c936ec8f25490dd033c31431aade20d3789b9d7
曹家玮 <529738530@qq.com>
2021-07-21 13:43:31 +0800

    feat: 动态规划

---
+++ 动态规划/[62]不同路径/solutions.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+/**
+ * dp[i][j] 是坐标 i,j的不同路径，
+ * dp[i][j] = dp[i -1][j] + dp[i][j -1 ]
+ */
+var uniquePaths = function (m, n) {
+  let dp = new Array(m).fill(new Array(n));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let i = 0; i < n; i++) {
+    dp[0][i] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+    }
+  }
+  return dp[m - 1][n - 1];
+};
+
+console.log(uniquePaths(3, 3));
